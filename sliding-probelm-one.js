f61656ffe5b3014b4251e6562b69628dbb993818
Vjkjouhari <vejouhari@gmail.com>
2025-01-05 22:52:01 +0530

    problem 1

--- sliding-probelm-one.js
+++ sliding-probelm-one.js
@@ -1,28 +1,6 @@
 // sliding windows Problems
 // Maximum Sum of Distinct Subarrays With Length K
 
-function maxSumSubarray(nums, k) {
-  let maxSum = 0;
-  let sum = 0;
-  for (let i = 0; i < k; i++) {
-    sum += nums[i];
-    maxSum = sum;
-  }
-
-  for (let i = k; i < nums.length; i++) {
-    sum += arr[i] - arr[i - k];
-    if (sum > maxSum) {
-      maxSum = sum;
-    }
-  }
-  return maxSum;
-}
-
-// let arr = [1, 5, 4, 2, 9, 9, 9];
-// // 1,5,4   5 4 2, 4,2,9  2,9,9   9,9,9
-// let k = 3;
-// console.log(maxSumSubarray(arr, k));
-
 function maxSumSubArrayDistinctEle(arr, k) {
   let freq = {};
   let maxSum = 0;
@@ -60,6 +38,5 @@ function maxSumSubArrayDistinctEle(arr, k) {
 }
 
 let arr = [1, 5, 2, 1, 4, 2, 4, 8, 9, 8, 9];
-// 1,5,4   5 4 2, 4,2,9  2,9,9   9,9,9
 let k = 3;
 console.log(maxSumSubArrayDistinctEle(arr, k));
